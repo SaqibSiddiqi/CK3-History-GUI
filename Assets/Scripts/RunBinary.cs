6b59929550bec9e02a1edd1a6839a8ed8813344c
SaqibSiddiqi <lookfright@gmail.com>
2026-04-14 17:28:24 +0530

    Refactor 1
    Refactor the code

--- Assets/Scripts/RunBinary.cs
+++ Assets/Scripts/RunBinary.cs
@@ -13,80 +13,99 @@ using UnityEngine.DedicatedServer;
 public class RunBinary : MonoBehaviour
 {
 
-    //UI Variables
-    [SerializeField]
-    TMP_Text saveFileText;
-    [SerializeField]
-    TMP_Dropdown languageDropdown;
-    [SerializeField]
-    TMP_Dropdown depthDropdown;
+    #region Variables
 
-    [SerializeField]
-    Toggle dumpToggle;
-    [SerializeField]
-    Button dumpButton;
+    // UI Text Variables
+    [SerializeField] TMP_Text saveFileText; // Text to display the selected save file name
+    [SerializeField] TMP_InputField consoleText; // Text to display the console output
+    // UI Dropdown Variables
+    [SerializeField] TMP_Dropdown languageDropdown; // Dropdown to select the language
+    [SerializeField] TMP_Dropdown depthDropdown; // Dropdown to select the depth
 
-    [SerializeField]
-    Toggle visToggle;
+    // UI Toggle Variables
+    [SerializeField] Toggle dumpToggle; // Toggle to enable or disable the --dump flag
+    [SerializeField] Toggle visToggle; // Toggle to enable or disable the --no-vis flag
+    [SerializeField] Toggle devModeToggle; // Toggle to enable or disable the --no-interaction flag
+    [SerializeField] Toggle templateToggle; // Toggle to enable or disable the --use-internal-templates flag
 
-    [SerializeField]
-    Toggle devModeToggle;
-    [SerializeField]
-    GameObject devPanel;
+    // UI Button Variables
+    [SerializeField] Button dumpButton; // Button to run the dump command
 
-    [SerializeField]
-    Toggle templateToggle;
+    // UI Panel Variables
+    [SerializeField] GameObject devPanel; // Panel to show the dev options
 
-    [SerializeField]
-    TMP_Text consoleText;
-
-
-
-    //File Variables
-    string filename;
-    string output;
-    string gamePath;
-    string include;
-    string gitPath;
-    string executablePath = "REDACTED_PATH_TO_EXECUTABLE"; // Path to the executable, this will be set by the user through the UI
-    string jsonDump = @"\dump.json"; // The name of the dump file, this will be appended to the dumpData path when the --dump flag is used
+    //Filepath Variables
+    string filename; // The path to the save file
+    string output; // The path to the output folder
+    string gamePath; // The path to the game folder
+    string include; // The path to the mods folder
+    string gitPath; // The path to the git folder
+    string executablePath; // Path to the executable
+    string jsonDump; // The name of the dump file
 
     //Output Variables 
-    int depth = 3;
-    string language = "english";
-    bool noVis;
+    int depth; // The depth of the output
+    string language; // The language of the output
+    bool noVis; // The bool function to disable images
 
     //Debug Variables
-    bool dump;
-    string dumpData;
-    bool noInteraction;
-    bool useInternal;
+    bool dump; // The bool function to enable the --dump flag {NOT WORKING}
+    string dumpData; // The path to the dump data folder {NOT WORKING}
+    
+    bool noInteraction; // The bool function to enable the --no-interaction flag {Always enabled}
+    bool useInternal; // The bool function to enable the --use-internal-templates flag
 
-    // Nullify the variables to prevent errors, they will be set by the user through the UI
-    void Awake()
+    #endregion
+
+    #region Common Functions
+    void Awake()        // This function is called when the script is loaded, it will define the variables and set the default values
     {
-        // Default values
+        DefineArgs();
+        DefineSerialized();
+    }
+
+    void DefineSerialized()
+    {
+        // This function is used to define the serialized variables
+        saveFileText = GameObject.Find("SaveFileText").GetComponent<TMP_Text>();
+        consoleText = GameObject.Find("ConsoleText").GetComponent<TMP_InputField>();
+        languageDropdown = GameObject.Find("LanguageDropdown").GetComponent<TMP_Dropdown>();
+        depthDropdown = GameObject.Find("DepthDropdown").GetComponent<TMP_Dropdown>();
+        dumpToggle = GameObject.Find("DumpToggle").GetComponent<Toggle>();
+        visToggle = GameObject.Find("VisToggle").GetComponent<Toggle>();
+        devModeToggle = GameObject.Find("DevModeToggle").GetComponent<Toggle>();
+        templateToggle = GameObject.Find("TemplateToggle").GetComponent<Toggle>();
+        dumpButton = GameObject.Find("DumpButton").GetComponent<Button>();
+        devPanel = GameObject.Find("DevPanel");
+
+        dumpButton.interactable = false; // Disable the dump button until the dump toggle is enabled
+    }
+
+    void DefineArgs()
+    {
+        // Default values of arguments
         filename = null;
-        depth = 3;
-        language = "english";
+        output = null;
         gamePath = null;
         include = null;
-        output = ".";
+        gitPath = null;
+        executablePath = null;
+        jsonDump = @"\dump.json";
+
+        depth = 3;
+        language = "english";
+        noVis = false;
+
         dump = false;
         dumpData = null;
-        noVis = false;
-        noInteraction = false;
+
+        noInteraction = true;
+
         useInternal = false;
-
-        dumpButton.interactable = false;
     }
+    #endregion
 
-    // Button functions to set the variables, these will be called by the UI buttons
-    public void GetInfo()
-    {
-        filename = saveFileText.text;
-    }
-
+    #region UI Button Functions
     public void OpenSaveDialog()
     {
         StartCoroutine(SaveDialog());
@@ -102,8 +121,8 @@ public class RunBinary : MonoBehaviour
         StartCoroutine(GamePathDialog());
     }
 
-    public void OpenDumpPathDialog() 
-    { 
+    public void OpenDumpPathDialog()
+    {
         StartCoroutine(DumpData());
     }
 
@@ -111,7 +130,7 @@ public class RunBinary : MonoBehaviour
     {
         StartCoroutine(GitFolder());
     }
-    public void SetLanguage() 
+    public void SetLanguage()
     {
         language = languageDropdown.options[languageDropdown.value].text;
         language.ToLower();
@@ -124,7 +143,7 @@ public class RunBinary : MonoBehaviour
 
     public void SetDumpButton()
     {
-        if(dumpToggle.isOn)
+        if (dumpToggle.isOn)
         {
             dump = true;
             dumpButton.interactable = true;
@@ -135,6 +154,9 @@ public class RunBinary : MonoBehaviour
             dumpButton.interactable = false;
         }
     }
+    #endregion
+
+    #region UI Toggle Functions
 
     public void checkVisToggle()
     {
@@ -173,7 +195,9 @@ public class RunBinary : MonoBehaviour
             useInternal = false;
         }
     }
+    #endregion
 
+    #region Main
     public void runCLI()
     {
         executablePath = Path.Combine(Application.streamingAssetsPath, "CK3 Extractor", "Binary", "ck3_history_extractor.exe");
@@ -213,7 +237,7 @@ public class RunBinary : MonoBehaviour
         consoleText.text = "Running CLI with arguments: " + string.Join(" ", startInfo.ArgumentList);
 
         var process = new Process { StartInfo = startInfo };
-        
+
         process.Start();
 
         consoleText.text += "\nProcess started with PID: " + process.Id + "A terminal should be running";
@@ -224,7 +248,7 @@ public class RunBinary : MonoBehaviour
 
         if (!string.IsNullOrEmpty(CLIerr))
         {
-            Debug.LogError("Error: " + CLIerr); 
+            Debug.LogError("Error: " + CLIerr);
             consoleText.text += "\nError: " + CLIerr;
         }
         else
@@ -233,6 +257,9 @@ public class RunBinary : MonoBehaviour
             consoleText.text += "\nOutput: " + CLIout;
         }
     }
+    #endregion
+
+    #region Sub Functions
     static string CleanSaveFileName(string fullPath)
     {
         // Get just the filename without directory and extension
@@ -243,7 +270,9 @@ public class RunBinary : MonoBehaviour
 
         return cleaned;
     }
+    #endregion
 
+    #region Dialog Functions
     // Coroutines to open the file dialogs, these will be called by the button functions
     IEnumerator SaveDialog()
     {
@@ -268,9 +297,9 @@ public class RunBinary : MonoBehaviour
 
     }
 
-    IEnumerator OutputDialog() 
+    IEnumerator OutputDialog()
     {
-        
+
         yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Folders, false, null, null, "Select Output Folder", "Select");
 
         if (FileBrowser.Success)
@@ -293,7 +322,7 @@ public class RunBinary : MonoBehaviour
         //Run the executable with the given parameters to dump the data, then read the output and store it in the dumpData variable
         yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Folders, false, null, null, "Select Dump Folder", "Select");
         if (FileBrowser.Success)
-        { 
+        {
             dumpData = FileBrowser.Result[0];
         }
 
@@ -307,5 +336,6 @@ public class RunBinary : MonoBehaviour
             string gitPath = FileBrowser.Result[0];
         }
     }
+    #endregion
 
 }
